631101d20a4210dc50b7f283fa25863f33b03d9e
vinit_kumar <vinit@iplayee.com>
2018-04-13 02:34:05 +0530

    added simple store actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import AppActions from "../actions/AppActions";
-import AppStore from '../stores/AppStore';
+import AppStore from "../stores/AppStore";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+    this._onChange = this._onChange.bind(this);
+  }
   handleClick() {
-    AppActions.addItem("this is the item");
+    AppActions.addItem("New Item");
+  }
+  _onChange() {
+    let items = AppStore.getAll();
+    this.setState({ items: items });
+  }
+  componentWillMount() {
+    AppStore.addChangeListener(this._onChange);
   }
   render() {
     return (
       <div className="App">
-        <h3 onClick={this.handleClick}>Click this Title, then check console</h3>
+        <button onClick={this.handleClick}>Click this</button>
+        {this.state.items.map(item => {
+          return <div>{item}</div>;
+        })}
       </div>
     );
   }
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,16 +1,31 @@
 import AppDispatcher from "../dispatcher/AppDispatcher";
 import { EventEmitter } from "events";
 
+let _items = [];
 const CHANGE_EVENT = "change";
 
+function addItem(article) {
+  _items.push(article);
+}
+
 class AppStore extends EventEmitter {
+  constructor() {
+    super();
+    this.dispatchToken = AppDispatcher.register(payload => {
+      addItem(payload.action.item);
+      this.emitChange();
+      return true;
+    });
+  }
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+  getAll() {
+    return _items;
+  }
   emitChange() {
     this.emit(CHANGE_EVENT);
   }
 }
-
-AppDispatcher.register(payload => {
-  console.log(payload)
-});
 
 export default new AppStore();
